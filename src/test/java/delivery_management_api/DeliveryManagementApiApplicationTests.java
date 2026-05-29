80c97b8b8e9b73da8536f1905b5a831ca15da9e8
Marcelo Justin <marcelojustin3@gmail.com>
2026-05-29 18:49:45 -0300

    feat: configure PostgreSQL datasource

---
+++ src/test/java/delivery_management_api/DeliveryManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package delivery_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
