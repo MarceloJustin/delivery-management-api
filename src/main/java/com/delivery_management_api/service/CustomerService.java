0fdb102874d73bf3b4b8a9feca5f6907cd74b37d
Marcelo Justin <marcelojustin3@gmail.com>
2026-05-30 17:35:57 -0300

    feat: handle customer not found exception

--- src/main/java/com/delivery_management_api/service/CustomerService.java
+++ src/main/java/com/delivery_management_api/service/CustomerService.java
@@ -9,6 +9,7 @@ import com.delivery_management_api.dto.CreateCustomerRequest;
 import com.delivery_management_api.dto.CustomerResponse;
 import com.delivery_management_api.dto.UpdateCustomerRequest;
 import com.delivery_management_api.entity.Customer;
+import com.delivery_management_api.exception.CustomerNotFoundException;
 import com.delivery_management_api.repository.CustomerRepository;
 
 @Service
@@ -36,12 +37,12 @@ public class CustomerService {
 	}
 
 	public CustomerResponse findCustomerById(Long id) {
-		Customer customer = customerRepository.findById(id).orElseThrow();
+		Customer customer = customerRepository.findById(id).orElseThrow(() -> new CustomerNotFoundException(id));
 		return new CustomerResponse(customer.getId(), customer.getName(), customer.getEmail());
 	}
 	
 	public CustomerResponse updateCustomer(Long id, UpdateCustomerRequest request) {
-		Customer customer = customerRepository.findById(id).orElseThrow();
+		Customer customer = customerRepository.findById(id).orElseThrow(() -> new CustomerNotFoundException(id));
 		customer.setName(request.getName());
 		customer.setEmail(request.getEmail());
 		Customer updatedCustomer = customerRepository.save(customer);
