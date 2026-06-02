3ea3cc2c922c3247524870d94d47363583b8f47d
Marcelo Justin <marcelojustin3@gmail.com>
2026-06-02 16:55:58 -0300

    feat: add validation error handling

--- src/main/java/com/delivery_management_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/delivery_management_api/exception/GlobalExceptionHandler.java
@@ -1,13 +1,17 @@
 package com.delivery_management_api.exception;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.delivery_management_api.dto.ErrorResponse;
+import com.delivery_management_api.dto.ValidationErrorResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -29,6 +33,32 @@ public class GlobalExceptionHandler {
 		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
+	
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	
+	public ResponseEntity<ValidationErrorResponse> handleValidationErrors(MethodArgumentNotValidException ex) {
+
+	    Map<String, String> validationErrors = new HashMap<>();
+
+	    ex.getBindingResult()
+	            .getFieldErrors()
+	            .forEach(error -> validationErrors.put(
+	                    error.getField(),
+	                    error.getDefaultMessage()));
+
+	    ValidationErrorResponse response =
+	            new ValidationErrorResponse(
+	                    LocalDateTime.now(),
+	                    HttpStatus.BAD_REQUEST.value(),
+	                    HttpStatus.BAD_REQUEST.getReasonPhrase(),
+	                    "Validation failed",
+	                    validationErrors);
+
+	    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+	            .body(response);
+	}
+	
 	
 	@ExceptionHandler(OrderCancellationNotAllowedException.class)
 	
