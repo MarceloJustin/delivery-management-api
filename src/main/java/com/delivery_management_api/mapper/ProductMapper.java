4734de0f47c64f55e34dcb4e5f4139bf432396bd
Marcelo Justin <marcelojustin3@gmail.com>
2026-07-08 16:38:05 -0300

    refactor: extract Product entity-to-DTO mapping into ProductMapper

---
+++ src/main/java/com/delivery_management_api/mapper/ProductMapper.java
@@ -0,0 +1,16 @@
+package com.delivery_management_api.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.delivery_management_api.dto.ProductResponse;
+import com.delivery_management_api.entity.Product;
+
+@Component
+public class ProductMapper {
+
+	public ProductResponse toResponse(Product product) {
+		return new ProductResponse(product.getId(), product.getName(), product.getPrice(),
+				product.getRestaurant().getId(), product.getRestaurant().getName());
+	}
+
+}
