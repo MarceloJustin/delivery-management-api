b81cbe9aad0c58d2f1889308848e5f13256d3261
Marcelo Justin <marcelojustin3@gmail.com>
2026-07-09 16:51:22 -0300

    feat: add findByUser query to CustomerRepository

--- src/main/java/com/delivery_management_api/repository/CustomerRepository.java
+++ src/main/java/com/delivery_management_api/repository/CustomerRepository.java
@@ -1,9 +1,14 @@
 package com.delivery_management_api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.delivery_management_api.entity.Customer;
+import com.delivery_management_api.entity.User;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	Optional<Customer> findByUser(User user);
 
 }
